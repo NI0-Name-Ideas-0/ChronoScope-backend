b8a2659f9ae0b218e89f3ae3b29e7d91e632672e
LordElias <kerlinelias@gmail.com>
2026-04-26 00:18:53 +0200

    fix: avoid StackOverflowException in TaskGraphNode.hashCode

--- src/main/java/de/ni0/chronoscope/algorithm/TaskGraphNode.java
+++ src/main/java/de/ni0/chronoscope/algorithm/TaskGraphNode.java
@@ -9,6 +9,18 @@ import de.ni0.chronoscope.model.DynamicTask;
 public record TaskGraphNode(DynamicTask task, List<TaskGraphNode> dependencies, List<TaskGraphNode> dependents) {
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TaskGraphNode other)) return false;
+        return this.task.getId() == other.task.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Long.hashCode(this.task.getId());
+    }
+
+    @Override
     public String toString() {
         return Long.toString(this.task.getId());
     }
