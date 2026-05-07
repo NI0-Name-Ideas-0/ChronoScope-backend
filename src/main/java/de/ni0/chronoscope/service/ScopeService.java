9131399ebd394a8baad2080713ecbb1dc3ba2491
Felix <junk@felix.weglehner.info>
2026-05-07 14:22:33 +0200

    feature: Implemented scopes-controller and service to access scopes of identity
    Fixed difficulty tests

--- src/main/java/de/ni0/chronoscope/service/ScopeService.java
+++ src/main/java/de/ni0/chronoscope/service/ScopeService.java
@@ -1,10 +1,23 @@
 package de.ni0.chronoscope.service;
 
+import de.ni0.chronoscope.model.Identity;
+import de.ni0.chronoscope.model.Scope;
+import de.ni0.chronoscope.repository.ScopeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 /**
  * Service placeholder for future planned-scope read operations.
  */
 @Service
+@RequiredArgsConstructor
 public class ScopeService {
+    private final ScopeRepository scopeRepository;
+
+    public Set<Scope> getScopes(Identity identity) {
+        return this.scopeRepository.getScopesByDynamicTaskIdentityId(identity.getId());
+    }
+
 }
